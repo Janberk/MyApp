1f3964fc8c0a3408be16610a7eeb862b7b798d31
canberk.demirkan <canberk.demirkan@gmail.com>
2013-12-24 18:18:12 +0100

    bla

--- de.canberk.uni_pro/src/de/canberk/uni_pro/SignUpActivity.java
+++ de.canberk.uni_pro/src/de/canberk/uni_pro/SignUpActivity.java
@@ -1,15 +1,72 @@
 package de.canberk.uni_pro;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignUpActivity extends Activity {
+
+	private EditText et_firstname;
+	private EditText et_lastname;
+	private EditText et_username;
+	private EditText et_email;
+	private EditText et_password;
+	private Button btn_signup;
+	private TextView tv_login_link;
+
+	private String firstname;
+	private String lastname;
+	private String username;
+	private String email;
+	private String password;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sign_up_screen);
+		initElements();
+
+		btn_signup.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Email und Passwort checken und einloggen.
+
+			}
+		});
+
+		tv_login_link.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), MainActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+
+	public void initElements() {
+		et_firstname = (EditText) findViewById(R.id.et_firstname);
+		et_lastname = (EditText) findViewById(R.id.et_lastname);
+		et_username = (EditText) findViewById(R.id.et_username);
+		et_email = (EditText) findViewById(R.id.et_email);
+		et_password = (EditText) findViewById(R.id.et_password);
+		btn_signup = (Button) findViewById(R.id.btn_signup);
+		tv_login_link = (TextView) findViewById(R.id.tv_login_link);
+	}
+	
+	public void readInput() {
+		firstname = et_firstname.getText().toString();
+		lastname = et_lastname.getText().toString();
+		username = et_username.getText().toString();
+		email = et_email.getText().toString();
+		password = et_password.getText().toString();
 	}
 
 	@Override
